3dcf9fc51f192be35efeb94ccf54e97ea2d45477
Smit <smitshah95@hotmail.com>
2017-05-02 13:04:34 +0800

    more questions solutions

---
+++ src/com/exam/problems/Operators.java
@@ -0,0 +1,9 @@
+package com.exam.problems;
+
+public class Operators {
+	public static void main(String[] args){
+		System.out.println((2>1) && (5<1) ||((5>3) || (1>2)));
+		double x = 2/3;
+		System.out.println(x);
+	}
+}
