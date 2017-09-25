edc604ba4e2d76224981d0802645cff468ec928d
Smit <smitshah95@hotmail.com>
2017-09-25 16:24:02 +0800

    removed redundancy

--- src/simpleobjects/HalfVerticalBounceDrop.java
+++ src/simpleobjects/HalfVerticalBounceDrop.java
@@ -14,13 +14,16 @@ public class HalfVerticalBounceDrop extends Drop {
 	public void move(int width, int height) {
 		
 			if(ypos == height){
+				
 				if(halfHeight == 0){
 					previousHalf = halfHeight = height/2;
 				}
 				else{
-					int half =  previousHalf/2;
-					previousHalf = half;
-					if(half+halfHeight < height)  halfHeight = half+halfHeight;
+					previousHalf =  previousHalf/2;
+					
+					if(previousHalf+halfHeight < height) {
+						halfHeight = previousHalf+halfHeight;
+					}
 				}
 				goUp = true;
 			}
